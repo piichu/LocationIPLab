edc877b23705b270b3008b027aeb97f388cbd6e3
DESKTOP-QJEE94R\victort <allvdda@gmail.com>
2024-02-25 15:52:18 +0300

    fixed hardcoded ip

--- src/main/java/com/example/locationip/service/LocationService.java
+++ src/main/java/com/example/locationip/service/LocationService.java
@@ -6,9 +6,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class LocationService {
     public Location getLocationByIPAddress(String ipAddress){
-        if (ipAddress.equals("192.168.0.0")) {
+        if (ipAddress.equals("255.255.255.255")) {
             return new Location(ipAddress, "Belarus", "Minsk");
-        } else if (ipAddress.equals("10.0.0.1")) {
+        } else if (ipAddress.equals("0.0.0.0")) {
             return new Location(ipAddress, "Russia", "Khabarovsk");
         } else {
             return new Location(ipAddress, "Belarus", "Baranovichi");
