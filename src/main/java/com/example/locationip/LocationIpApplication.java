2b7a34b7c324336c975eac4c7dbbb0231239b72e
DESKTOP-QJEE94R\victort <allvdda@gmail.com>
2024-02-25 15:25:15 +0300

    request params

---
+++ src/main/java/com/example/locationip/LocationIpApplication.java
@@ -0,0 +1,13 @@
+package com.example.locationip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationIpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocationIpApplication.class, args);
+    }
+
+}
